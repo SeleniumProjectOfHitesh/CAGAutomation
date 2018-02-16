bff2c3b12e5bf56adbcb4d88a5a2aa00e315bdff
Hitesh <hitesh.k.lalwani11@gmail.com>
2018-02-16 20:43:55 +0530

    Commiting Results Page

---
+++ src/test/java/pageObject/CC_Results_Page.java
@@ -0,0 +1,19 @@
+package pageObject;
+
+import org.openqa.selenium.WebDriver;
+import org.testng.Assert;
+
+public class CC_Results_Page extends AbstractPage{
+
+	public CC_Results_Page(WebDriver driver) {
+		super(driver);
+		
+
+}
+	public void verifyURL()
+	{
+		Assert.assertEquals(driver.getCurrentUrl(), "https://www.moneyhero.com.hk/en/credit-card/best-deals/results");
+		   
+	}
+	
+}
